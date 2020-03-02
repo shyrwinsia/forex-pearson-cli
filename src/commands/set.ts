9cdf9679ffdbf9365ead83f99d8af782a49d1e34
Shyrwinsteel Sia <shyrwin69@gmail.com>
2020-03-02 11:53:36 +0800

    Added scaffolding

---
+++ src/commands/set.ts
@@ -0,0 +1,9 @@
+import { Command } from "@oclif/command";
+
+class Set extends Command {
+  static description = "sets an item on the watchlist";
+
+  async run() {}
+}
+
+export = Set;
---
+++ src/commands/unset.ts
@@ -0,0 +1,9 @@
+import { Command } from "@oclif/command";
+
+class Unset extends Command {
+  static description = "unsets an item on the watchlist";
+
+  async run() {}
+}
+
+export = Unset;
